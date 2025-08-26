438996604aad08d2bedbfb41ae78cdff96093b5e
weiweihuang93 <weiwei.huang.93@gmail.com>
2025-08-26 16:13:57 +0800

    feat: 新增 TodoList 頁面，完成基礎 CRUD 功能 - 取得代辦事項列表 - 新增、刪除代辦事項 - 登入驗證與登出功能 - 尚未加入狀態切換與篩選功能

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,4 +1,113 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { computed, onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+const router = useRouter()
+
+const todoToken = ref('')
+const nickname = ref('')
+
+// 驗證登入
+onMounted(async () => {
+  const cookieToken = document.cookie.replace(
+    /(?:(?:^|.*;\s*)todoToken\s*\=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  )
+  if (!cookieToken) return router.push('/login')
+  todoToken.value = cookieToken
+
+  try {
+    const res = await axios.get(`${BASE_URL}/users/checkout`, {
+      headers: { Authorization: todoToken.value },
+    })
+    nickname.value = res.data.nickname
+    await getTodos()
+  } catch (error) {
+    console.log('驗證失敗', error)
+    router.push('/login')
+  }
+})
+
+// 登出
+const signOut = async () => {
+  try {
+    const res = await axios.post(`${BASE_URL}/users/sign_out`, null, {
+      headers: { Authorization: todoToken.value },
+    })
+    todoToken.value = ''
+    document.cookie = 'todoToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/'
+    router.push('/login')
+  } catch (error) {
+    console.log('登出失敗', error)
+  }
+}
+
+// 代辦事項
+const todos = ref([])
+
+const getTodos = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/todos/`, {
+      headers: { Authorization: todoToken.value },
+    })
+    todos.value = res.data.data
+  } catch (error) {
+    console.log('代辦事項載入失敗', error)
+  }
+}
+
+const newTodo = ref({
+  content: '',
+})
+
+// 新增代辦事項
+const addTodo = async () => {
+  if (!newTodo.value.content.trim()) {
+    console.log('欄位未填寫')
+    return
+  }
+  try {
+    const res = await axios.post(`${BASE_URL}/todos/`, newTodo.value, {
+      headers: { Authorization: todoToken.value },
+    })
+    await getTodos()
+    newTodo.value.content = ''
+  } catch (error) {
+    console.log('新增失敗', error)
+  }
+}
+
+// 刪除代辦事項
+const removeTodo = async (todo_id) => {
+  try {
+    const res = await axios.delete(`${BASE_URL}/todos/${todo_id}`, {
+      headers: { Authorization: todoToken.value },
+    })
+    await getTodos()
+  } catch (error) {
+    console.log('刪除失敗', error)
+  }
+}
+
+// 切換完成狀態
+const toggleStatus = async (todo_id) => {
+  try {
+    const res = await axios.patch(`${BASE_URL}/todos/${todo_id}/toggle`, null, {
+      headers: { Authorization: todoToken.value },
+    })
+    await getTodos()
+  } catch (error) {
+    console.log('更新失敗', error)
+  }
+}
+
+const totalUndoneTodo = computed(() => {
+  return todos.value.filter((todo) => !todo.status).length
+})
+</script>
 
 <template>
   <div id="todoListPage" class="bg-half">
@@ -6,16 +115,20 @@
       <h1><a href="#">ONLINE TODO LIST</a></h1>
       <ul>
         <li class="todo_sm">
-          <a href="#"><span>王小明的代辦</span></a>
+          <a href="#">
+            <span>{{ nickname || '○○○' }} 的代辦</span>
+          </a>
         </li>
-        <li><a href="#loginPage">登出</a></li>
+        <li>
+          <a @click.prevent="signOut" href="#">登出</a>
+        </li>
       </ul>
     </nav>
-    <div class="conatiner todoListPage vhContainer">
+    <div class="container todoListPage vhContainer">
       <div class="todoList_Content">
         <div class="inputBox">
-          <input type="text" placeholder="請輸入待辦事項" />
-          <a href="#">
+          <input v-model="newTodo.content" type="text" placeholder="請輸入待辦事項" />
+          <a @click.prevent="addTodo" href="#">
             <i class="fa fa-plus"></i>
           </a>
         </div>
@@ -27,63 +140,23 @@
           </ul>
           <div class="todoList_items">
             <ul class="todoList_item">
-              <li>
+              <li v-for="todo in todos" :key="todo.id">
                 <label class="todoList_label">
-                  <input class="todoList_input" type="checkbox" value="true" />
-                  <span>把冰箱發霉的檸檬拿去丟</span>
+                  <input
+                    type="checkbox"
+                    class="todoList_input"
+                    :checked="todo.status"
+                    @change="toggleStatus(todo.id)"
+                  />
+                  <span>{{ todo.content }}</span>
                 </label>
-                <a href="#">
-                  <i class="fa fa-times"></i>
-                </a>
-              </li>
-              <li>
-                <label class="todoList_label">
-                  <input class="todoList_input" type="checkbox" value="true" />
-                  <span>打電話叫媽媽匯款給我</span>
-                </label>
-                <a href="#">
-                  <i class="fa fa-times"></i>
-                </a>
-              </li>
-              <li>
-                <label class="todoList_label">
-                  <input class="todoList_input" type="checkbox" value="true" />
-                  <span>整理電腦資料夾</span>
-                </label>
-                <a href="#">
-                  <i class="fa fa-times"></i>
-                </a>
-              </li>
-              <li>
-                <label class="todoList_label">
-                  <input class="todoList_input" type="checkbox" value="true" />
-                  <span>繳電費水費瓦斯費</span>
-                </label>
-                <a href="#">
-                  <i class="fa fa-times"></i>
-                </a>
-              </li>
-              <li>
-                <label class="todoList_label">
-                  <input class="todoList_input" type="checkbox" value="true" />
-                  <span>約vicky禮拜三泡溫泉</span>
-                </label>
-                <a href="#">
-                  <i class="fa fa-times"></i>
-                </a>
-              </li>
-              <li>
-                <label class="todoList_label">
-                  <input class="todoList_input" type="checkbox" value="true" />
-                  <span>約ada禮拜四吃晚餐</span>
-                </label>
-                <a href="#">
+                <a @click.prevent="removeTodo(todo.id)" href="#">
                   <i class="fa fa-times"></i>
                 </a>
               </li>
             </ul>
             <div class="todoList_statistics">
-              <p>5 個已完成項目</p>
+              <p>{{ totalUndoneTodo }} 個待完成項目</p>
             </div>
           </div>
         </div>
