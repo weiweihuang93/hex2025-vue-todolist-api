84dc727b32085836437cbcad89d1f03b30fb711e
weiweihuang93 <weiwei.huang.93@gmail.com>
2025-08-26 15:46:45 +0800

    feat: 新增註冊與登入頁面、表單驗證 - 新增對應路由配置 - 引入 axios 進行 API 請求 - 使用 .env 管理 API URL - 新增表單驗證（空值檢查與密碼一致性檢查） - 調整錯誤樣式，命名為 errorMsg

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,4 +1,59 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+const router = useRouter()
+
+// 登入用資料
+const loginForm = ref({
+  email: '',
+  password: '',
+})
+
+// 錯誤狀態
+const errors = ref({
+  email: '',
+  password: '',
+  nickname: '',
+  confirmPassword: '',
+})
+
+const todoToken = ref('')
+
+const signIn = async () => {
+  // 重置錯誤訊息
+  errors.value = { email: '', password: '' }
+
+  // 空值檢查
+  if (!loginForm.value.email.trim()) {
+    errors.value.email = '此欄位不可留空'
+  }
+  if (!loginForm.value.password.trim()) {
+    errors.value.password = '此欄位不可留空'
+  }
+
+  // 如果有任何錯誤就直接 return
+  if (errors.value.email || errors.value.password) return
+
+  try {
+    const res = await axios.post(`${BASE_URL}/users/sign_in`, loginForm.value)
+
+    todoToken.value = res.data.token
+    const expireDate = new Date(res.data.exp * 1000)
+    document.cookie = `todoToken=${todoToken.value}; expires=${expireDate.toUTCString()}; path=/`
+
+    loginForm.value = {}
+    console.log(`您好，${res.data.nickname} 登入成功！`)
+
+    router.push('/todolist')
+  } catch (error) {
+    console.log('註冊失敗', error)
+  }
+}
+</script>
 
 <template>
   <div id="loginPage" class="bg-yellow">
@@ -17,34 +72,32 @@
         />
       </div>
       <div>
-        <form class="formControls" action="index.html">
+        <form @submit.prevent="signIn" class="formControls">
           <h2 class="formControls_txt">最實用的線上代辦事項服務</h2>
           <label class="formControls_label" for="email">Email</label>
           <input
+            v-model="loginForm.email"
             class="formControls_input"
             type="text"
             id="email"
             name="email"
             placeholder="請輸入 email"
-            required
           />
-          <span>此欄位不可留空</span>
+          <span v-if="errors.email" class="errorMsg">{{ errors.email }}</span>
+
           <label class="formControls_label" for="pwd">密碼</label>
           <input
+            v-model="loginForm.password"
             class="formControls_input"
             type="password"
             name="pwd"
             id="pwd"
             placeholder="請輸入密碼"
-            required
           />
-          <input
-            class="formControls_btnSubmit"
-            type="button"
-            onclick="javascript:location.href='#todoListPage'"
-            value="登入"
-          />
-          <a class="formControls_btnLink" href="#signUpPage">註冊帳號</a>
+          <span v-if="errors.password" class="errorMsg">{{ errors.password }}</span>
+
+          <input class="formControls_btnSubmit" type="submit" value="登入" />
+          <RouterLink to="signup" class="formControls_btnLink">註冊帳號</RouterLink>
         </form>
       </div>
     </div>
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,4 +1,85 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { computed, ref, watch } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+const router = useRouter()
+
+// 註冊用資料
+const registerForm = ref({
+  email: '',
+  password: '',
+  nickname: '',
+})
+
+const confirmPassword = ref('')
+
+// 錯誤狀態
+const errors = ref({
+  email: '',
+  password: '',
+  nickname: '',
+  confirmPassword: '',
+})
+
+//
+const validateConfirmPassword = computed(() => {
+  // 如果任一個還沒輸入，就不需要判斷一致性
+  if (!registerForm.value.password || !confirmPassword.value) {
+    return ''
+  }
+  // 兩者都輸入了，再判斷是否一致
+  if (registerForm.value.password !== confirmPassword.value) {
+    return '密碼不一致'
+  }
+  return ''
+})
+
+watch(validateConfirmPassword, (val) => {
+  errors.value.confirmPassword = val
+})
+
+const signUp = async () => {
+  // 重置錯誤訊息
+  errors.value = { email: '', password: '', nickname: '', confirmPassword: '' }
+
+  // 空值檢查
+  if (!registerForm.value.email.trim()) {
+    errors.value.email = '此欄位不可留空'
+  }
+  if (!registerForm.value.password.trim()) {
+    errors.value.password = '此欄位不可留空'
+  }
+  if (!registerForm.value.nickname.trim()) {
+    errors.value.nickname = '此欄位不可留空'
+  }
+  if (!confirmPassword.value.trim()) {
+    errors.value.confirmPassword = '此欄位不可留空'
+  }
+
+  // 如果有任何錯誤就直接 return
+  if (
+    errors.value.email ||
+    errors.value.password ||
+    errors.value.nickname ||
+    errors.value.confirmPassword
+  )
+    return
+
+  try {
+    const res = await axios.post(`${BASE_URL}/users/sign_up`, registerForm.value)
+    registerForm.value = { email: '', password: '', nickname: '' }
+    confirmPassword.value = ''
+    console.log('註冊成功')
+
+    router.push('/login')
+  } catch (error) {
+    console.log('註冊失敗', error)
+  }
+}
+</script>
 
 <template>
   <div id="signUpPage" class="bg-yellow">
@@ -17,50 +98,54 @@
         />
       </div>
       <div>
-        <form class="formControls" action="index.html">
+        <form @submit.prevent="signUp" class="formControls">
           <h2 class="formControls_txt">註冊帳號</h2>
           <label class="formControls_label" for="email">Email</label>
           <input
+            v-model="registerForm.email"
             class="formControls_input"
             type="text"
             id="email"
             name="email"
             placeholder="請輸入 email"
-            required
           />
+          <span v-if="errors.email" class="errorMsg">{{ errors.email }}</span>
+
           <label class="formControls_label" for="name">您的暱稱</label>
           <input
+            v-model="registerForm.nickname"
             class="formControls_input"
             type="text"
             name="name"
             id="name"
             placeholder="請輸入您的暱稱"
           />
+          <span v-if="errors.nickname" class="errorMsg">{{ errors.nickname }}</span>
+
           <label class="formControls_label" for="pwd">密碼</label>
           <input
+            v-model="registerForm.password"
             class="formControls_input"
             type="password"
             name="pwd"
             id="pwd"
             placeholder="請輸入密碼"
-            required
           />
-          <label class="formControls_label" for="pwd">再次輸入密碼</label>
+          <span v-if="errors.password" class="errorMsg">{{ errors.password }}</span>
+
+          <label class="formControls_label" for="confirmpwd">再次輸入密碼</label>
           <input
+            v-model="confirmPassword"
             class="formControls_input"
             type="password"
-            name="pwd"
-            id="pwd"
+            name="confirmpwd"
+            id="confirmpwd"
             placeholder="請再次輸入密碼"
-            required
           />
-          <input
-            class="formControls_btnSubmit"
-            type="button"
-            onclick="javascript:location.href='#todoListPage'"
-            value="註冊帳號"
-          />
-          <a class="formControls_btnLink" href="#loginPage">登入</a>
+          <span v-if="errors.confirmPassword" class="errorMsg">{{ errors.confirmPassword }}</span>
+
+          <input class="formControls_btnSubmit" type="submit" value="註冊帳號" />
+          <RouterLink to="login" class="formControls_btnLink">登入</RouterLink>
         </form>
       </div>
     </div>
