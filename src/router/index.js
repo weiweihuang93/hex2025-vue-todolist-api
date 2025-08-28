36baa7b6426450f2a5a1a6e8cc36953939dd87fd
weiweihuang93 <weiwei.huang.93@gmail.com>
2025-08-28 16:05:08 +0800

    chore: 將 Vue Router 改為 hash 模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SignupPage from '@/views/SignupPage.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import TodoList from '@/views/TodoList.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', redirect: '/login' }, // 這行就是設定預設路徑
+    { path: '/', redirect: '/login' }, // 設定預設路徑
     {
       path: '/signup',
       component: SignupPage,
